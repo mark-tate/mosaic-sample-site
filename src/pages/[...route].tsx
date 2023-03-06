47d2a9a436e661606331ccaf205e119db4d6a286
Mark Tate <143323+mark-tate@users.noreply.github.com>
2023-03-06 23:51:28 +0000

    remove edge

--- src/pages/[...route].tsx
+++ src/pages/[...route].tsx
@@ -9,10 +9,6 @@ import {
 
 import type { MyAppProps, MyMiddlewareProps } from '../types/mosaic';
 
-export const config = {
-  runtime: 'experimental-edge'
-};
-
 /**
  * Extend props passed to MyApp by adding your own middleware ('withMyExampleMiddleware') functions.
  *
